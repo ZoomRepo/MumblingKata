e6dc9564e16b9825205fe65b312cb431d21e52f8
ZoomRepo <ollybeumkes@hotmail.co.uk>
2022-03-22 22:01:16 +0000

    Initial Refactor

--- MumblingKata.Tests/MumblingKataTest.cs
+++ MumblingKata.Tests/MumblingKataTest.cs
@@ -4,15 +4,23 @@ namespace MumblingKata.Tests
 {
     public class MumblingKataTest
     {
+        private readonly MumblingKata _mumbling = new MumblingKata();
         [Fact]
         public void Mumble_Letters_ShouldReturnA_WhenGivena()
         {
-            //Arrange
-            MumblingKata mumbling = new MumblingKata();
-
             //Act
-            var result = mumbling.Mumble_Letters("a");
+            var result = _mumbling.Mumble_Letters("a");
            
+            //Assert
+            Assert.Equal("A", result);
+        }
+
+        [Fact]
+        public void Mumble_Letters_Givenab_ShouldReturnA_Bb()
+        {
+            //Act
+            var result = _mumbling.Mumble_Letters("a");
+
             //Assert
             Assert.Equal("A", result);
         }
